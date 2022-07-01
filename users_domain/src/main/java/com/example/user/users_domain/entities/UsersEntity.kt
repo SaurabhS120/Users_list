6b8e81ed38c1b202c7b003ce19333d5fecd4e02b
SaurabhS120 <saurabh.sonar120@gmail.com>
2022-07-01 13:20:51 +0530

    Dummy profile image and full name layout

--- users_domain/src/main/java/com/example/user/users_domain/entities/UsersEntity.kt
+++ users_domain/src/main/java/com/example/user/users_domain/entities/UsersEntity.kt
@@ -3,4 +3,5 @@ package com.example.user.users_domain.entities
 data class UsersEntity(val id:Int, val firstName:String, val maidenName:String, val lastName:String)
 {
     fun idString() = id.toString()
+    fun fullName() = "$firstName $maidenName $lastName"
 }
