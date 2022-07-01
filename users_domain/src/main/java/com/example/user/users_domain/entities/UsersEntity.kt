ea46b11d6371edb3fe50105942c34eb47e196bf7
SaurabhS120 <saurabh.sonar120@gmail.com>
2022-07-01 12:25:13 +0530

    User details showing id first, median and last name

--- users_domain/src/main/java/com/example/user/users_domain/entities/UsersEntity.kt
+++ users_domain/src/main/java/com/example/user/users_domain/entities/UsersEntity.kt
@@ -1,3 +1,6 @@
 package com.example.user.users_domain.entities
 
 data class UsersEntity(val id:Int, val firstName:String, val maidenName:String, val lastName:String)
+{
+    fun idString() = id.toString()
+}
